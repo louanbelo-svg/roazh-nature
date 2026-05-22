9f01828c47277b1148bcd3be187b440aa6a43139
louanbelo-svg <louanbelo@gmail.com>
2026-05-22 20:15:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const fades = document.querySelectorAll('.fade');
 
+const observer = new IntersectionObserver(entries => {
+
+  entries.forEach(entry => {
+
+    if(entry.isIntersecting){
+      entry.target.classList.add('visible');
+    }
+
+  });
+
+}, {
+  threshold:0.15
+});
+
+fades.forEach(fade => {
+  observer.observe(fade);
+});
